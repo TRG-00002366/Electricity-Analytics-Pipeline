31977ac4e64f01b2534ebc9c28fcf0b5eb8611bc
JustinE821 <Justinevans124@outlook.com>
2026-03-03 16:49:49 -0500

    Added function to request data from the eia api

---
+++ code/data_collection/api_call.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def create_temp_file():
+    # 1. Define the API endpoint URL
+    url = f'https://api.eia.gov/v2/electricity/rto/fuel-type-data/data/?api_key={}&frequency=hourly&data[0]=value&start=2026-02-22T00&end=2026-02-28T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000' 
+
+    # 2. Make a GET request to the URL
+    response = requests.get(url)
+
+    # 3. Check the status code and process the response
+    if response.status_code == 200:
+        # Parse the JSON data into a Python dictionary
+        data = response.json()
+        #print("Data received:", data)
+        with open("./data/temp_api_data.json", 'w') as json_file:
+            json.dump(data, json_file)
+
+    else:
+        print(f"Request failed with status code: {response.status_code}")
---
+++ code/main.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from data_collection.api_call import create_temp_file
+
+def main():
+    print("Foo")
+#   function below is used to create a temporary json file out of the api query
+#    create_temp_file()
+
+    
+    pass
+
+if __name__ == "__main__":
+    main()
